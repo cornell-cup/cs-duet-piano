84c681f2879b4cfe08dcce5dc32a2c25ad98a17d
lauralindy <laura.f.lin@gmail.com>
2017-11-11 13:37:19 -0500

    renamed dejavu file

---
+++ src/audioRec.py
@@ -0,0 +1,23 @@
+from dejavu import Dejavu
+from dejavu.recognize import MicrophoneRecognizer
+
+config = {
+	"database": {
+		"host": "127.0.0.1",
+		"user": "root",
+		"passwd": "root",
+		"db": "dejavu",
+	},
+	"database_type": "mysql",
+	"fingerprint_limit": 15
+}
+
+def recognizeAudio():
+	djv = Dejavu(config)
+
+	djv.fingerprint_directory("songs", [".mp3"], 3)
+
+	song = djv.recognize(MicrophoneRecognizer, seconds=10)
+
+	print(song)
+	return song['song_name']
