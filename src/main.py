d4ec2ed89f8c7093996962cbcae67c4edc87a543
lauralindy <laura.f.lin@gmail.com>
2017-12-02 12:09:41 -0500

    update

--- src/main.py
+++ src/main.py
@@ -7,13 +7,14 @@ import mido
 import math
 from threading import Thread
 
-noteOffset = 28 # are notes zero indexed?
+noteOffset = 28
+# are notes zero indexed?
 
 def hexToNote(hexcode):
 	hex = hexcode.encode("hex")
 	pressed = []
-	for i in range(len(hex)/4+1):
-		sub = hex[i*4:(i+1)*4] #3333
+	for i in range(len(hex) / 4 + 1):
+		sub = hex[i * 4:(i + 1) * 4]  # 3333
 		print sub
 		bin_sub = bin(int(sub, 16))[2:]
 		for key in bin_sub:
@@ -22,6 +23,7 @@ def hexToNote(hexcode):
 			else:
 				pressed.append(False)
 	return pressed
+
 
 def getUniqueNotes(lastTime, lastData, dataNotes):
 	nowTime = time.time()
@@ -32,21 +34,22 @@ def getUniqueNotes(lastTime, lastData, dataNotes):
 		##msg = Midi.Message()
 		for i in range(len(dataNotes)):
 			if dataNotes[i] == True and lastData[i] == False:
-				#messageType = 'note_on'
-				#msg = Midi.Message(messageType, note=(noteOffset + i), velocity=64, time=math.floor(time.time()))
+				# messageType = 'note_on'
+				# msg = Midi.Message(messageType, note=(noteOffset + i), velocity=64, time=math.floor(time.time()))
 				left, right = Midi.splitLR(dataNotes[i])
 				pressDown[0] += left
 				pressDown[1] += right
 
 			elif dataNotes[i] == False and lastData[i] == True:
-				#messageType = 'note_off'
-				#msg = Midi.Message(messageType, note=(noteOffset + i), velocity=64, time=math.floor(time.time()))
+				# messageType = 'note_off'
+				# msg = Midi.Message(messageType, note=(noteOffset + i), velocity=64, time=math.floor(time.time()))
 				left, right = Midi.splitLR(dataNotes[i])
 				letGo[0] += left
 				letGo[1] += right
 		lastData = dataNotes
 		lastTime = nowTime
 	return lastData, lastTime, pressDown, letGo
+
 
 '''
 Params: MIDI integer representation of the key that the pinkie should be on for either hand
@@ -58,8 +61,8 @@ def getPinkie(key):
 
 class Main:
 	def __init__(self):
-		self.time_current = time.time()*1000
-		self.time_start = time.time()*1000
+		self.time_current = time.time() * 1000
+		self.time_start = time.time() * 1000
 
 		self.left_played = []
 		self.left_index = 0
@@ -74,8 +77,8 @@ class Main:
 		self.human_transcript = []
 
 		self.human_side = ""
-		self.human_played = [[],[]]
-		self.human_letGo = [[],[]]
+		self.human_played = [[], []]
+		self.human_letGo = [[], []]
 		self.human_tempo = 0
 		self.tempo_scale = 1
 
@@ -83,13 +86,13 @@ class Main:
 		song = Rec.recognizeAudio()
 		self.transcript = Midi.transcribe(song)
 
-	def initial_match(self,spi):
-		set = Thread(target = self.setSong)
+	def initial_match(self, spi):
+		set = Thread(target=self.setSong)
 		set.start()
 		print "running simultaneously"
 		while self.transcript == []:
 			now = time.time()
-			lastData = [] # last note value, last timestamp
+			lastData = []  # last note value, last timestamp
 			lastTime = 0
 			while time.time() - now < 5:
 				data = spi.readbytes(n)
@@ -100,11 +103,12 @@ class Main:
 				if letGo != []:
 					self.human_letGo.append(letGo)
 
-		self.time_current = time.time()*1000
+		self.time_current = time.time() * 1000
 
 		print(self.transcript)
 
-		human_average = sum(self.human_played[0] + self.human_played[1])/len(self.human_played[0]+ self.human_played[1])
+		human_average = sum(self.human_played[0] + self.human_played[1]) / len(
+			self.human_played[0] + self.human_played[1])
 		if human_average > 64:
 			self.human_side = "R"
 			self.robot_transcript = self.transcript[0]
@@ -112,8 +116,8 @@ class Main:
 			self.human_side = "L"
 			self.human_transcript = self.transcript[1]
 
-		#based on time_current and notes played by human, get robot note
-		skipToL, skipToR = len(self.human_played[0]),len(self.human_played[1])
+		# based on time_current and notes played by human, get robot note
+		skipToL, skipToR = len(self.human_played[0]), len(self.human_played[1])
 		currNoteL, currNoteR = self.human_transcript[0][skipToL], self.human_transcript[0][skipToR]
 		if currNoteL[1] == self.human_played[0][-1] and currNoteR[1] == self.human_played[1][-1]:
 			left_time = currNoteL[0]
@@ -124,7 +128,6 @@ class Main:
 			for entry in self.robot_transcript[1]:
 				if entry[0] > right_time:
 					nextNoteR = entry
-
 
 	'''
 	Params: self.queue represented as [five oldest notes, current note, five next notes]
@@ -144,37 +147,38 @@ class Main:
 		print "Unimplemented"
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
 	process = Main()
 	spi = spidev.SpiDev()
-	bus = ""
-	device = ""
+	bus, device = "", ""
 	spi.open(bus, device)
 	process.initial_match(spi)
 	end = False
-	n = 0 # number of bytes
+	n = 0  # number of bytes
+
 	# get unique data from the spi thing
 	# convert hex to mido keys
 	# append to list that would store 5 seconds -> tempo matching () -> bpm -> 
 	# update tempo_scale = new bpm/old bpm
 	# continue match()
+
 	while not end:
 		tempoSample = mido.MidiFile()
 		tempoSamplePath = 'tempo.mid'
 		track = mido.MidiTrack()
 		now = time.time()
-		lastData = [] # last note value, last timestamp
+		lastData = []  # last note value, last timestamp
 		lastTime = 0
 		while time.time() - now < 5:
 			data = spi.readbytes(n)
 			dataNotes = hexToNote(data)
 			lastData, lastTime, pressDown, letGo = getUniqueNotes(lastTime, lastData, dataNotes)
-			if pressDown != [[],[]] or letGo != [[],[]]:
+			if pressDown != [[], []] or letGo != [[], []]:
 				process.continue_match(pressDown, letGo)
 		tempoSample.save(tempoSamplePath)
 		bpm = Tempo.get_tempo_bpm(tempoSamplePath)
 		process.human_tempo = bpm
-		#song reaches end and end is true then
-		# need end of song check somewhere
+	# song reaches end and end is true then
+	# need end of song check somewhere
 	spi.close()
 	process.continue_match()
