940093130d5dc04bbf6b4f0e456486182512859b
Emily Leng <eleng132@gmail.com>
2017-12-02 12:41:57 -0500

    merge

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import midi as Midi
 import audioRec as Rec
 import tempo as Tempo
 import time
-import spidev
 import mido
+import wiringpi
 import math
 from threading import Thread
 
@@ -149,14 +149,13 @@ class Main:
 
 if __name__ == "__main__":
 	process = Main()
-	spi = spidev.SpiDev()
-	bus, device = "", ""
-	spi.open(bus, device)
-	process.initial_match(spi)
-	end = False
-	n = 0  # number of bytes
-
-	# get unique data from the spi thing
+	process.intial_match()
+	#pins: 7 hand f,8 hand f,24 sensors,23 step
+	wiringpi.wiringPiSetupGpio()	
+	wiringpi.pinMode(7, 1)
+	wiringpi.pinMode(8, 1)
+	wiringpi.pinMode(23, 1)
+	wiringpi.pinMode(24, 1)
 	# convert hex to mido keys
 	# append to list that would store 5 seconds -> tempo matching () -> bpm -> 
 	# update tempo_scale = new bpm/old bpm
@@ -170,15 +169,30 @@ if __name__ == "__main__":
 		lastData = []  # last note value, last timestamp
 		lastTime = 0
 		while time.time() - now < 5:
-			data = spi.readbytes(n)
+			data = wriringpi.digitalRead(23) #pin 23
 			dataNotes = hexToNote(data)
 			lastData, lastTime, pressDown, letGo = getUniqueNotes(lastTime, lastData, dataNotes)
 			if pressDown != [[], []] or letGo != [[], []]:
 				process.continue_match(pressDown, letGo)
-		tempoSample.save(tempoSamplePath)
-		bpm = Tempo.get_tempo_bpm(tempoSamplePath)
-		process.human_tempo = bpm
-	# song reaches end and end is true then
-	# need end of song check somewhere
-	spi.close()
-	process.continue_match()
+			# song reaches end and end is true then
+			# need end of song check somewhere
+			nowTime = time.time()
+		
+			if dataNotes != lastData and math.floor(nowTime) != math.floor(lastTime):
+				msg = mido.Message()
+				for i in range (0, len(dataNotes)):
+					if dataNotes[i] == True and lastData[i] == False:
+						messageType = 'note_on'
+						msg = mido.Message(messageType, note=(noteOffset + i), velocity=64, time = math.floor(time.time()))
+						track.append(msg)
+					elif dataNotes[i] == False and lastData[i] == True:
+						messageType = 'note_off'
+						msg = mido.Message(messageType, note=(noteOffset + i), velocity=64, time = math.floor(time.time()))
+						track.append(msg)
+				lastData = dataNotes
+				lastTime = nowTime
+				process.continue_match(msg)
+				tempoSample.save(tempoSamplePath)
+				bpm = Tempo.get_tempo_bpm(tempoSamplePath)
+				process.human_tempo = bpm
+		process.continue_match()
